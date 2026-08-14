457e15115736a02053203e86a97978f3133976c1
benoitpaillard <ben.paillard@gmail.com>
2026-08-15 00:07:49 +0200

    Bump Lacanautics cache to v2.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lacanautics-v2';
+const CACHE='lacanautics-v2.1';
 const CORE=['./','./index.html','./hires.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
